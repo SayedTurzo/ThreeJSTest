65a95f9d970cb0f40831a0bfcb17a4d6f896a800
SayedTurzo <51632150+SayedTurzo@users.noreply.github.com>
2024-03-28 15:30:07 +0600

    added gui

--- ThreeJSTest/src/js/scripts.js
+++ ThreeJSTest/src/js/scripts.js
@@ -1,30 +1,23 @@
 ﻿import * as THREE from 'three';
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
-
+import * as dat from 'dat.gui';
 
 // Create a renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-
 // Create a scene and camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-
 const orbit = new OrbitControls(camera,renderer.domElement);
-
 
 camera.position.set(-10,30,30);
 orbit.update();
 
-
-
-
 const axesHelper =  new THREE.AxesHelper(3);
 scene.add(axesHelper);
-
 
 const planeGeo = new THREE.PlaneGeometry(30,30);
 const planeMat = new THREE.MeshBasicMaterial({color: 0x008000, side : THREE.DoubleSide});
@@ -32,17 +25,14 @@ const plane = new THREE.Mesh(planeGeo,planeMat);
 scene.add(plane);
 plane.rotation.x= -.5* Math.PI;
 
-
 const gridHelper = new THREE.GridHelper(30,60);
 scene.add(gridHelper);
-
 
 const boxGeo = new THREE.BoxGeometry();
 const boxMat = new THREE.MeshBasicMaterial({color: 0x00ff00});
 const box = new THREE.Mesh(boxGeo,boxMat);
 scene.add(box);
 box.position.set(0,0,0);
-
 
 const sphereGeo = new THREE.SphereGeometry(1,50,50);
 const sphereMat = new THREE.MeshLambertMaterial({color: 0x00ff00 , wireframe : false});
@@ -56,6 +46,8 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
 directionalLight.position.set(1, 1, 1).normalize(); // Set position
 scene.add(directionalLight);
 
+
+const gui =  new dat.GUI();
 
 
 
